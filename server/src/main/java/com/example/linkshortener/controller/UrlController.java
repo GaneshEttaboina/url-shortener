02b92a8efc0619a4776854fe8f73374632857d97
Ettaboina Ganesh <ganesh@example.com>
2025-07-12 10:14:17 +0530

    crossOrigin updated

--- server/src/main/java/com/example/linkshortener/controller/UrlController.java
+++ server/src/main/java/com/example/linkshortener/controller/UrlController.java
@@ -1,21 +1,31 @@
 package com.example.linkshortener.controller;
 
-import com.example.linkshortener.model.ShortUrl;
-import com.example.linkshortener.service.UrlService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-
-import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
-import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import com.example.linkshortener.model.ShortUrl;
+import com.example.linkshortener.service.UrlService;
 
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {
+    "http://localhost:3000",
+    "https://url-shortner-assessement.netlify.app"
+})
 public class UrlController {
 
     @Autowired
